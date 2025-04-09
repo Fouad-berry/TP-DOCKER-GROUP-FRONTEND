7179326a6889164029edbafa3c13acfacb5fb9d5
Fouad-berry <fidriss96@gmail.com>
2025-04-09 17:42:06 +0200

    [ADD] create another content for layout page

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,11 @@
-'use client';
+import React from 'react';
 
-import { useState } from 'react'
-
-export default function Home() {
-  const [search, setSearch] = useState('')
-
+const page = () => {
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
-      <div className="max-w-xl mx-auto bg-white p-6 rounded-2xl shadow-md">
-        <h1 className="text-2xl font-bold mb-4 text-center">Bienvenue sur le portail d'informations des Lycées numériques en France</h1>
-        
-        <input
-          type="text"
-          placeholder="Rechercher..."
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          className="w-full px-4 py-2 border rounded-xl mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-
-        <ul className="space-y-2">
-        </ul>
-      </div>
+    <div>
+      Page d'accueil
     </div>
-  )
-}
+  );
+};
+
+export default page;
