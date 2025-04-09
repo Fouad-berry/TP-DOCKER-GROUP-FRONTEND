97edc4492f0892c8e9e6ec0bc93e48a3d1073cef
Fouad-berry <fidriss96@gmail.com>
2025-04-09 16:27:29 +0200

    [ADD] add Title to the page

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 p-6">
       <div className="max-w-xl mx-auto bg-white p-6 rounded-2xl shadow-md">
-        <h1 className="text-2xl font-bold mb-4 text-center">Planètes</h1>
+        <h1 className="text-2xl font-bold mb-4 text-center">Bienvenue sur le portail d'informations des Lycées numériques en France</h1>
         
         <input
           type="text"
